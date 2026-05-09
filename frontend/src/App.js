fd91203552d11cd265dcbbf6b57b73b4aa112fb0
Forex Dev <dev@forex.local>
2026-05-09 13:29:01 +0000

    feat: add Leaderboard route to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Analytics from './pages/Analytics';
 import RedFlags from './pages/RedFlags';
+import Leaderboard from './pages/Leaderboard';
 import Navigation from './components/Navigation';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/analytics" element={<Analytics />} />
           <Route path="/red-flags" element={<RedFlags />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
         </Routes>
       </div>
     </Router>
